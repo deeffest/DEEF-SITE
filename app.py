cd41f4494f80d520fc24fa4d215c05e854a2ab32
deeffest <deeffestt@gmail.com>
2025-11-27 23:10:37 +0300

    Update codebase

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, redirect
 from flask_caching import Cache
 
 app = Flask(__name__)
-
 cache = Cache(app, config={"CACHE_TYPE": "simple", "CACHE_DEFAULT_TIMEOUT": 1800})
 
 apps = [
